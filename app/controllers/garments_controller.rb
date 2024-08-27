3946ef6900ec74510295bec91884c46b39a8ad6c
Ines Perez <inesperezgomez@outlook.com>
2024-08-27 14:57:47 +0100

    upload image functionality

--- app/controllers/garments_controller.rb
+++ app/controllers/garments_controller.rb
@@ -1,2 +1,24 @@
 class GarmentsController < ApplicationController
+  def new
+    @garment = Garment.new
+
+  end
+
+  def create
+    @user = current_user
+    @garment = Garment.new(garment_params)
+    @garment.user = @user
+    if @garment.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+end
+
+private
+
+def garment_params
+  params.require(:garment).permit(:text) #need to add photo
 end
