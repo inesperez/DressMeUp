704606fca84073c84fa61a79858770deecbecc8d
Helena Brauchitsch <helena.brauchitsch@gmail.com>
2024-08-28 19:13:01 +0200

    preferences html

---
+++ app/javascript/controllers/preferences_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="preferences"
+export default class extends Controller {
+  static targets = ["customOccasion", "occasion"];
+
+  connect() {
+    console.log("Hello from preferences")
+  }
+
+  fire(event) {
+
+    console.log(event)
+    const selectedValue = this.occasionTarget.value;
+    if (selectedValue === "Other") {
+      this.customOccasionTarget.classList.remove("d-none");
+    }
+  }
+}
