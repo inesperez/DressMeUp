357a778eab1281088d6e58af91cdeee8cc547a83
Helena Brauchitsch <helena.brauchitsch@gmail.com>
2024-09-03 16:37:20 +0200

    set up done

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reset-form"
 export default class extends Controller {
-  connect() {
-
-  }
   reset() {
     this.element.reset()
   }
