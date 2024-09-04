6e6de83ae05ee4ea075e324983c155e9992845fc
Oliver Petrus <petrus.oliver@gmail.com>
2024-09-04 16:10:42 +0100

    Redis

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -2,7 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="reset-form"
 export default class extends Controller {
+  connect() {
+    console.log('connected');
+  }
+
   reset() {
     this.element.reset()
+    console.log(this.element);
   }
 }
